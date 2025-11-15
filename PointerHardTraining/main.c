fb0ce7700ff3a28b45e5f87417827c10a74f01d2
권석기 <kwons3327@gmail.com>
2025-11-16 02:56:01 +0900

    Initial Commit

---
+++ PointerHardTraining/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  PointerHardTraining
+//
+//  Created by a on 11/16/25.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return EXIT_SUCCESS;
+}
